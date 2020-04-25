9e4626f345688b8e49a93cd9025cf03800777589
danc243 <40128477+danc243@users.noreply.github.com>
2020-04-25 16:34:31 -0500

    herencia con los botones

--- levels/main_menu/desktop_icon/desktop_icon.gd
+++ levels/main_menu/desktop_icon/desktop_icon.gd
@@ -1,13 +1,19 @@
 extends Button
 
-func _ready():
-	pass # Replace with function body.
 
+onready var label_name = $RichTextLabel
+
+func _ready():
+	label_name.text = name
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func icon_pressed():
+	print("Parent Script")
 
 func _on_Desktop_Icon_pressed():
-	print(name)
+	icon_pressed()
+
---
+++ levels/main_menu/game_icon/game_icon.gd
@@ -0,0 +1,8 @@
+extends "res://levels/main_menu/desktop_icon/desktop_icon.gd"
+
+
+func _ready():
+	pass
+
+func icon_pressed():
+	print("hijo")
