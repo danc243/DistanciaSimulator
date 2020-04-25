ab8fdd68b607a4efb64a5a85dd774354abb2d547
danc243 <40128477+danc243@users.noreply.github.com>
2020-04-25 16:09:44 -0500

    sdadsa

---
+++ levels/main_menu/desktop_icon/desktop_icon.gd
@@ -0,0 +1,13 @@
+extends Button
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Desktop_Icon_pressed():
+	print(name)
