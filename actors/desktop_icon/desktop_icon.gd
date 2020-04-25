8a3a57e9ca3ae507793f65775e87744ae5c26576
danc243 <40128477+danc243@users.noreply.github.com>
2020-04-25 13:54:18 -0500

    icono padre

---
+++ actors/desktop_icon/desktop_icon.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	
+	if event is InputEventMouseButton:
+		if event.is_pressed():	
+			print("Presionando ícono")
+		else:
+			print("Se dejó de presionar")
