783ceb7076e43e48bde2fe74f4fe3c1403bba8c0
Miguel Bonilla <jeff.mov@gmail.com>
2020-04-30 11:05:13 -0500

    Can remove game
    Bueno mira pues puse que con el esc se quite el juego lmao

--- levels/main_menu/main_menu.gd
+++ levels/main_menu/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 onready var nodeHandler = $handler
+var elNodoVentana = null
 
 func _ready():
 	pass
@@ -10,10 +11,15 @@ func _on_Game_Icon_on_pressed_button():
 		print("El juego ya está activo")
 	else:
 		var node = load("res://actors/window_game/window_container.tscn")
-		var instance = node.instance()
+		elNodoVentana = node.instance()
 		var ventana = get_tree().get_root().get_node("Control").get_node("handler")
-		ventana.add_child(instance)
+		ventana.add_child(elNodoVentana)
 
 
 func _on_Desktop_Icon_on_pressed_button():
 	pass
+
+func _input(event: InputEvent) -> void:
+	if(event.is_action_pressed("escape") && elNodoVentana!=null):
+		elNodoVentana.queue_free()
+		elNodoVentana = null
