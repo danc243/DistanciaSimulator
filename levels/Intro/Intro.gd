d759012547a58d52c981ffaae40d65eef809ab38
Miguel Bonilla <jeff.mov@gmail.com>
2020-05-02 20:23:59 -0500

    AAAAAAAA
    perdon pero ya funciona

--- levels/Intro/Intro.gd
+++ levels/Intro/Intro.gd
@@ -1,6 +1,14 @@
 extends Node
 
+onready var video = $VideoPlayer
+
+func _ready() -> void:
+	video.play()
 
 func _on_VideoPlayer_finished() -> void:
 	get_tree().change_scene("res://levels/main_menu/main_menu.tscn")
 	pass # Replace with function body.
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("action") || event.is_action_pressed("escape") :
+		get_tree().change_scene("res://levels/main_menu/main_menu.tscn")
